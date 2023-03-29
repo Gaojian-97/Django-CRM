66356eba334980c33ac4a2bdca15452bbc8e70b8
td gale gao <920056915@qq.com>
2023-03-29 13:48:09 +0800

    log in

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+	#Check to see if logging in
+	if request.method=='POST':
+		username=request.POST['username']
+		password=request.POST['password']
+		# Authenticate
+		user=authenticate(request,username=username,password=password)
+		if user is not None:
+			login(request,user)
+			messages.success(request,"恭喜你，登录成功")
+			return redirect('home')
+		else:
+			messages.success(request,"登录出现错误，请重新尝试")
+			return redirect('home')
+	else:		
+		return render(request,'home.html',{})
+
+def login_user(request):
+	pass
+
+def logout_user(request):
+	pass
