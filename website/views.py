1d2caf776cd639eb4ec7605e4b997b69324868e3
td gale gao <920056915@qq.com>
2023-03-29 16:58:21 +0800

    now,the all data can show in the homepage,but no other function such as add

--- website/views.py
+++ website/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +23,7 @@ def home(request):
             messages.success(request, "登录出现错误，请重新尝试")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {"records": records})
 
 
 def logout_user(request):
@@ -45,3 +48,13 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up Records
+        record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'record': record})
+    else:
+        messages.success(request,"You must be logged in to view this page")
+        return redirect('home')
